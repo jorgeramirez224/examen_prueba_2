5b4d3704da1b10273274f5450b4aa75191998450
jorgeramirez224 <50390461+jorgeramirez224@users.noreply.github.com>
2022-03-09 21:36:39 -0700

    resetear tamano y disminuir opacidad

--- lib/controllers/controller_zoom_image.dart
+++ lib/controllers/controller_zoom_image.dart
@@ -2,6 +2,7 @@ import 'package:get/get.dart';
 
 class ControllerZoomImage extends GetxController {
 double sizeImg = 125.0;
+double opa = 1.0;
 
   void increaseZoomImage () {
     sizeImg = sizeImg+10;
@@ -11,5 +12,15 @@ double sizeImg = 125.0;
   void decreaseZoomImage () {
     sizeImg = sizeImg-10;
     update();
-  } 
+  }
+
+  void resetZoomImage () {
+    sizeImg = 125.0;
+    update();
+  }
+
+  void opacityImage () {
+      opa -= 0.2;
+    update();
+  }
 }
--- lib/pages/zoom_image.dart
+++ lib/pages/zoom_image.dart
@@ -13,34 +13,61 @@ class ZoomImage extends StatelessWidget {
         backgroundColor: Colors.yellow,
         title: const Text('Imagen Zoom actividad'),
       ),
-      body:  Center(
+      body: Center(
         child: GetBuilder<ControllerZoomImage>(
-          builder: (controllerZoomImage) =>  Container(
-            child:  CircleAvatar(
+          builder: (controllerZoomImage) => Container(
+            child: CircleAvatar(
               radius: controllerZoomImage.sizeImg,
-              backgroundColor: Colors.yellow,
+              backgroundColor:
+                  Colors.yellow.withOpacity(controllerZoomImage.opa),
               backgroundImage: const NetworkImage(
-                'https://upload.wikimedia.org/wikipedia/commons/0/0a/DK_logo_-_red_border.png'),
+                  'https://upload.wikimedia.org/wikipedia/commons/0/0a/DK_logo_-_red_border.png'),
             ),
           ),
         ),
       ),
-      floatingActionButton: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      floatingActionButton: Wrap(
+        direction: Axis.vertical,
         children: [
-          FloatingActionButton(
-            onPressed: () {
-              controllerZoomImage.increaseZoomImage();
-            },
+          Container(
+            margin: const EdgeInsets.all(10),
+            child: FloatingActionButton(
+              onPressed: () {
+                controllerZoomImage.increaseZoomImage();
+              },
               child: const Icon(Icons.add),
               backgroundColor: Colors.blue,
+            ),
           ),
-          FloatingActionButton(
-            onPressed: () {
-              controllerZoomImage.decreaseZoomImage();
-            },
-            child:  const Icon(Icons.remove),
-            backgroundColor: Colors.red,
+          Container(
+            margin: const EdgeInsets.all(10),
+            child: FloatingActionButton(
+              onPressed: () {
+                controllerZoomImage.decreaseZoomImage();
+              },
+              child: const Icon(Icons.remove),
+              backgroundColor: Colors.red,
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.all(10),
+            child: FloatingActionButton(
+              onPressed: () {
+                controllerZoomImage.resetZoomImage();
+              },
+              child: const Icon(Icons.restore),
+              backgroundColor: Colors.grey,
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.all(10),
+            child: FloatingActionButton(
+              onPressed: () {
+                controllerZoomImage.opacityImage();
+              },
+              child: const Icon(Icons.opacity),
+              backgroundColor: Colors.green,
+            ),
           ),
         ],
       ),
